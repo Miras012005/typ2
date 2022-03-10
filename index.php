8b026275635224c72b508472b48a9aab8eb4f6f5
Miroslaw_Socha <mirek.socha24680@wp.pl>
2022-03-10 09:17:40 +0100

    php-show and css

--- index.php
+++ index.php
@@ -27,6 +27,67 @@
             <button type="submit">Wyświetl</button>
         </div>
     </form>
+    <div class="show">
+        <?php
+        if (isset($_POST['option_show']) && !empty($_POST['records'])) {
+            $option_show = $_POST['option_show'];
+            $records = $_POST['records'];
+            $conn = mysqli_connect('localhost', 'root', '', 'dane1');
+
+            $q = "select imie, nazwisko, rok_urodzenia from osoby";
+            $result = mysqli_query($conn, $q);
+            $max_records = mysqli_num_rows($result);
+
+            $q = "select imie, nazwisko, rok_urodzenia from osoby limit $records";
+            $result = mysqli_query($conn, $q);
+            $current_records = mysqli_num_rows($result);
+
+            echo "<h4>Wyświetlono $current_records z $max_records rekordów</h4>";
+            echo "<div class='show_data'>";
+            switch ($option_show) {
+                case 'list':
+                    echo "<ol>";
+                    while ($row = mysqli_fetch_array($result)) {
+                        $age = date('Y') - $row[2];
+                        echo "<li>Imię: {$row[0]}, Nazwisko: {$row[1]}, Wiek: $age</li>";
+                    }
+                    echo "</ol>";
+                    break;
+                case 'table':
+                    $lp = 1;
+                    echo "<table>
+                            <tr>
+                                <th>Lp.</th>
+                                <th>Imię</th>
+                                <th>Nazwisko</th>
+                                <th>Wiek</th>
+                            </tr>";
+                    while ($row = mysqli_fetch_array($result)) {
+                        $age = date('Y') - $row[2];
+                        echo "<tr>
+                            <td>{$lp}.</td>
+                            <td>{$row[0]}</td>
+                            <td>{$row[1]}</td>
+                            <td>{$age}</td>
+                        </tr>";
+                        $lp++;
+                    };
+                    echo "</table>";
+                    break;
+                case 'cards':
+                    while ($row = mysqli_fetch_array($result)) {
+                        $age = date('Y') - $row[2];
+                        echo "<div><h3>Imię i nazwisko:<br> {$row[0]} {$row[1]}</h3> Wiek: {$age}</div>";
+                    }
+                    break;
+            }
+            echo "</div>";
+        } else {
+            echo "<h4>Brak rekordów do wyświetlenia!</h4>";
+        }
+
+        ?>
+    </div>
 </body>
 
 </html>
